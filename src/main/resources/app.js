d9122a54b11fd6a3635db89141f71c4213f6a097
Jonathan Buch <jonathan.buch@gmail.com>
2012-12-22 18:04:30 +0100

    Move configuration of the NetServer to app.js
    * filter messages, don't allow all

--- src/main/resources/app.js
+++ src/main/resources/app.js
@@ -4,11 +4,17 @@ var webServerConf = {
     port: 8080,
     host: 'localhost'
 };
+var netServerConf = {
+    port: 8081,
+    host: "localhost",
 
-// Start the web server, with the config we defined above
+    eventbusConfig: {prefix: "/eventbus"},
+    allowIncoming: [],
+    allowOutgoing: [{adress_re: "tcp\\..+"}]
+};
 
 vertx.deployModule('vertx.web-server-v1.0', webServerConf);
 
 vertx.deployVerticle('de.jbuch.play.vertx.EchoServer', null, 1, function() {});
-vertx.deployVerticle('de.jbuch.play.vertx.NetServer', null, 1, function() {});
+vertx.deployVerticle('de.jbuch.play.vertx.NetServer', netServerConf, 1, function() {});
 vertx.deployVerticle('de.jbuch.play.vertx.TickerServer', null, 1, function() {});
